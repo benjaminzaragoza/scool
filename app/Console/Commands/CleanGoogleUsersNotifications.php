c8dde86e118ea6b582d19c8a198fda0827f59d4c
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-07-11 12:24:58 +0200

    Sync

--- app/Console/Commands/CleanGoogleUsersNotifications.php
+++ app/Console/Commands/CleanGoogleUsersNotifications.php
@@ -3,30 +3,31 @@
 namespace App\Console\Commands;
 
 use App\GoogleGSuite\GoogleDirectory;
+use App\Models\GoogleNotification;
 use App\Models\GoogleWatch;
 use Config;
 use Illuminate\Console\Command;
 
 /**
- * Class CleanGoogleUserswatch.
+ * Class CleanGoogleUsersNotifications.
  *
  * @package App\Console\Commands
  */
-class CleanGoogleUserswatch extends Command
+class CleanGoogleUsersNotifications extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'google-users:clean-watch {tenant}';
+    protected $signature = 'google-users:clean-notifications {tenant}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Clean google users watch';
+    protected $description = 'Clean google users notifications';
 
     /**
      * Create a new command instance.
@@ -46,7 +47,7 @@ class CleanGoogleUserswatch extends Command
     public function handle()
     {
         apply_tenant($this->argument('tenant'));
-        GoogleWatch::truncate();
-        $this->info('Table google_watches truncated ok!');
+        GoogleNotification::truncate();
+        $this->info('Table google_notifications truncated ok!');
     }
 }
