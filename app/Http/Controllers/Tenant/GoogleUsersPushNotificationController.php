8c0a2b787bc9d30c473dc955ebf1552fde386b89
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-07-11 11:52:55 +0200

    Sync

--- app/Http/Controllers/Tenant/GoogleUsersPushNotificationController.php
+++ app/Http/Controllers/Tenant/GoogleUsersPushNotificationController.php
@@ -23,12 +23,13 @@ class GoogleUsersPushNotificationController extends Controller
      */
     public function store(Request $request)
     {
+        $expiration = GoogleNotification::getExpiration($request);
         $notification = GoogleNotification::create([
             'channel_id' => GoogleNotification::getChannelId($request),
             'channel_type' => GoogleNotification::getType($request),
             'token' => GoogleNotification::getToken($request),
             'message_number' => GoogleNotification::getMessageNumber($request),
-            'expiration_time' => Carbon::createFromTimestampMs(GoogleNotification::getExpiration($request))
+            'expiration_time' => is_numeric($expiration) ? Carbon::createFromTimestampMs($expiration) : null
         ]);
         if (GoogleNotification::validate($request)) {
             event(new GoogleUserNotificationReceived($request));
