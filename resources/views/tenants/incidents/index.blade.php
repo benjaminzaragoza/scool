57badb6ce77fc8e83e959d88f83329866b8c5d45
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-10-18 17:29:42 +0200

    Sync

--- resources/views/tenants/incidents/index.blade.php
+++ resources/views/tenants/incidents/index.blade.php
@@ -8,7 +8,7 @@
     <v-container fluid grid-list-md text-xs-center>
         <v-layout row wrap>
             <v-flex xs12>
-                <incidents-list incidents="{{ array() }}"></incidents-list>
+                <incidents-list :incidents="{{ $incidents }}"></incidents-list>
             </v-flex>
         </v-layout>
     </v-container>
