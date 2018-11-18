ddadd43cbbbb33557b6522fbcec00a787443a7ec
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-11-18 20:33:31 +0100

    Sync

--- app/Listeners/Incidents/IncidentLogger.php
+++ app/Listeners/Incidents/IncidentLogger.php
@@ -21,7 +21,7 @@ class IncidentLogger
     public static function stored($event)
     {
         Log::create([
-            'text' => "Ha creat la incidència $event->incident->subject",
+            'text' => 'Ha creat la incidència ' . $event->incident->subject,
             'time' => $event->incident->created_at,
             'action_type' => 'store',
             'module_type' => 'Incidents',
--- tests/Unit/Tenants/Changelog/IncidentsLoggerTest.php
+++ tests/Unit/Tenants/Changelog/IncidentsLoggerTest.php
@@ -4,7 +4,10 @@ namespace Tests\Unit\Tenants;
 
 use App\Console\Kernel;
 use App\Listeners\Incidents\IncidentLogger;
+use App\Models\Incident;
 use App\Models\Log;
+use App\Models\User;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -32,24 +35,28 @@ class IncidentsLoggerTest extends TestCase
     }
 
     /** @test */
-    public function todo()
+    public function stored()
     {
+        $incident= Incident::create([
+            'subject' => 'No funciona res aula 20',
+            'description' => 'Bla bla bla',
+        ]);
+        $incident->assignUser($user = factory(User::class)->create([]));
+
         $event = (Object) [
-            'credentials' => [
-                'email' => 'prova@gmail.com'
-            ]
+            'incident' => $incident
         ];
-        IncidentLogger::incorrectAttempt($event);
+        IncidentLogger::stored($event);
         $log = Log::first();
-        $this->assertEquals($log->text,"Intent de login incorrecte amb l'usuari <strong>prova@gmail.com</strong>");
+        $this->assertEquals($log->text,'Ha creat la incidència No funciona res aula 20');
         $this->assertNotNull($log->time);
-        $this->assertEquals($log->user_id, null);
-        $this->assertEquals($log->action_type,'error');
-        $this->assertEquals($log->module_type,'UsersManagment');
-        $this->assertEquals($log->loggable_id,null);
-        $this->assertEquals($log->loggable_type,null);
-        $this->assertNull($log->persistedLoggable);
-        $this->assertEquals($log->icon,'error');
-        $this->assertEquals($log->color,'error');
+        $this->assertEquals($log->user_id, $user->id);
+        $this->assertEquals($log->action_type,'store');
+        $this->assertEquals($log->module_type,'Incidents');
+        $this->assertEquals($log->loggable_id,$incident->id);
+        $this->assertEquals($log->loggable_type,Incident::class);
+        $this->assertNotNull($log->persistedLoggable);
+        $this->assertEquals($log->icon,'add');
+        $this->assertEquals($log->color,'success');
     }
 }
