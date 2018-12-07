ba8f1874e8c901c39fb1d0406e53f7268edfa064
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-12-07 13:29:01 +0100

    Sync

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-12-07 13:27:20.
+    * Generated on 2018-12-07 13:28:18.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
