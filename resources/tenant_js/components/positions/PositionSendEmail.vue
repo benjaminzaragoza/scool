106a0ff296f46bd764cf69e17c9fa91b1b0b5f99
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-15 10:03:10 +0100

    Sync

--- resources/tenant_js/components/positions/PositionSendEmail.vue
+++ resources/tenant_js/components/positions/PositionSendEmail.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn color="primary" flat title="Enviar email" icon @click="send">
+    <v-btn color="primary" flat title="Enviar email" icon @click="send" :loading="loading" :disabled="loading || position.users.length === 0">
         <v-icon>email</v-icon>
     </v-btn>
 </template>
@@ -19,8 +19,18 @@ export default {
     }
   },
   methods: {
-    send () {
-      console.log('TODO SEND')
+    async send () {
+      let res = await this.$confirm("Segur que voleu tornar a enviar l'email al usuari/usuaris?", { title: 'Esteu segurs?', buttonTrueText: 'Enviar' })
+      if (res) {
+        this.loading = true
+        window.axios.get('/api/v1/positions/' + this.position.id + '/email/send').then((response) => {
+          this.$snackbar.showMessage('Email enviat correctament')
+          this.loading = false
+        }).catch((error) => {
+          this.$snackbar.showError(error)
+          this.loading = false
+        })
+      }
     }
   }
 }
