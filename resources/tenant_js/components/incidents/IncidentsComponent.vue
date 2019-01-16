348c52136a1b2c7e78fecca1d9bbb15d40dc850c
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-16 15:42:48 +0100

    Sync

--- resources/tenant_js/components/incidents/IncidentsComponent.vue
+++ resources/tenant_js/components/incidents/IncidentsComponent.vue
@@ -1,57 +1,88 @@
 <template>
     <span id="incidents_component">
-        <floating-add v-model="dialog" title="Nova incidència">
+        <floating-add :show="dataIncidents.length > 0" v-model="dialog" title="Nova incidència">
             <incident-add @close="dialog = false"></incident-add>
         </floating-add>
-        <v-container fluid grid-list-md text-xs-center>
-            <v-layout row wrap>
-                <v-flex xs12>
-                    <incidents-list :incidents="incidents" :incident="incident" :incident-users="incidentUsers" :manager-users="managerUsers" :tags="tags"></incidents-list>
-                </v-flex>
-            </v-layout>
-        </v-container>
+        <span v-if="dataIncidents.length > 0">
+            <v-container fluid grid-list-md text-xs-center>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <incidents-list :incidents="dataIncidents" :incident="incident" :incident-users="incidentUsers" :manager-users="managerUsers" :tags="tags"></incidents-list>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </span>
+        <span v-else>
+            <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+                  <v-flex xs12 md6 offset-md3 class="mt-3">
+                      <div class="grey lighten-3" style="width: 240px;height: 240px;padding: 60px;border-radius: 50%;display: inline-flex;">
+                            <svg :style="'width:120px;height:120px;fill: ' + this.$vuetify.theme.primary.base + ';'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6.47 9.8A5 5 0 0 1 .2 3.22l3.95 3.95 2.82-2.83L3.03.41a5 5 0 0 1 6.4 6.68l10 10-2.83 2.83L6.47 9.8z"/></svg>
+                      </div>
+                  </v-flex>
+                  <v-flex xs12 md4 offset-md4 class="mt-3">
+                      <h1 class="display-1 grey--text text--darken-2"><strong>Uups!</strong> No hi ha cap incidència... teniu el luxe 😛 de poder ser els primers en:</h1>
+                  </v-flex>
+                  <v-flex xs12 md6 offset-md3 class="mt-3">
+                      <v-btn large color="accent" @click="dialog = true">
+                          <v-icon left dark>add</v-icon>
+                          Crear una incidència
+                      </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+        </span>
+
     </span>
 </template>
 
 <script>
-export default {
-  name: 'Incidents',
-  data () {
-    return {
-      dialog: false
-    }
-  },
-  props: {
-    incidents: {
-      type: Array,
-      default: function () {
-        return undefined
+  import * as mutations from '../../store/mutation-types'
+
+  export default {
+    name: 'Incidents',
+    data () {
+      return {
+        dialog: false
       }
     },
-    incident: {
-      type: Object,
-      default: function () {
-        return undefined
+    props: {
+      incidents: {
+        type: Array,
+        required: true
+      },
+      incident: {
+        type: Object,
+        default: function () {
+          return undefined
+        }
+      },
+      incidentUsers: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      },
+      managerUsers: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      },
+      tags: {
+        type: Array,
+        default: function () {
+          return []
+        }
       }
     },
-    incidentUsers: {
-      type: Array,
-      default: function () {
-        return []
+    computed: {
+      dataIncidents () {
+        return this.$store.getters.incidents
       }
     },
-    managerUsers: {
-      type: Array,
-      default: function () {
-        return []
-      }
-    },
-    tags: {
-      type: Array,
-      default: function () {
-        return []
-      }
+    created () {
+      this.$store.commit(mutations.SET_INCIDENTS, this.incidents)
     }
   }
-}
 </script>
--- resources/views/tenants/home.blade.php
+++ resources/views/tenants/home.blade.php
@@ -1,110 +1,104 @@
 @extends('tenants.layouts.app')
 
 @section('content')
-
-    <v-container fluid grid-list-md text-xs-center>
-        <v-layout row wrap>
+    <v-container
+            fill-height
+            fluid
+            grid-list-xl
+            text-xs-center
+    >
+        <v-layout wrap row>
             <v-flex xs12>
-                <v-toolbar color="blue darken-3">
-                    <v-toolbar-side-icon class="white--text"></v-toolbar-side-icon>
-                    <v-toolbar-title class="white--text title">Dades professorat i places</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn icon class="white--text" >
-                        <v-icon>settings</v-icon>
-                    </v-btn>
-                    <v-btn icon class="white--text">
-                        <v-icon>refresh</v-icon>
-                    </v-btn>
-                </v-toolbar>
-                <v-container fluid grid-list-md text-xs-center>
-                    <v-layout row wrap>
-                        <v-flex xs3>
-                            <bar :labels="{{ $teacherTotals }}"
-                                 :data="{{ $teacherTotalsData }}"
-                                 :colors ="{{ $teacherTotalsColors }}"
-                                 title="Totals professors"></bar>
-                        </v-flex>
-                        <v-flex xs2>
-                            <donut :labels="{{ $teacherTypes }}"
-                                   :data="{{ $teacherTypesData }}"
-                                   title="Tipus de places"></donut>
-                        </v-flex>
-                        <v-flex xs2>
-{{--                            <donut :labels="{{ $tipusJornades }}" title="Places per tipus Jornada"></donut>--}}
-                        </v-flex>
-                        <v-flex xs6>
-                            <donut :labels="{{ $specialties->keys() }}"
-                                   :data="{{ $specialties->values() }}"
-                                   title="Places per especialitats"></donut>
-                        </v-flex>
-                        <v-flex xs6>
-                            <donut :labels="{{ $families->keys() }}"
-                                   :data="{{ $families->values() }}"
-                                   title="Places per famílies"></donut>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
+                <v-layout align-center justify-center>
+                    <v-flex sm12
+                            md8
+                    >
+                        <h1 class="display-3 grey--text text--darken-2 mt-3">&#128075; Benvingut!</h1>
+                        <h3 class="headline grey--text mt-5">Escolliu quin tipus de relació voleu tenir amb la nostra comunitat educativa:</h3>
+                        <v-layout align-center justify-center row wrap class="mt-5">
+                            <v-flex sm12
+                                    md4
+                            >
+                                <material-card class="v-card-profile ma-5">
+                                    <v-avatar
+                                            slot="offset"
+                                            class="mx-auto d-block"
+                                            size="130"
+                                    >
+                                        <img
+                                                src="/img/userTypes/student.jpg"
+                                        >
+                                    </v-avatar>
+                                    <v-card-text class="text-xs-center">
+                                        <v-btn
+                                                large
+                                                color="success"
+                                                round
+                                        >Alumne</v-btn>
+                                    </v-card-text>
+                                </material-card>
+                            </v-flex>
+                            <v-flex xs12
+                                    sm12
+                                    md4
+                            >
+                                <material-card class="v-card-profile ma-5">
+                                    <v-avatar
+                                            slot="offset"
+                                            class="mx-auto d-block"
+                                            size="130"
+                                    >
+                                        <img
+                                                src="/img/userTypes/teacher.jpg"
+                                        >
+                                    </v-avatar>
+                                    <v-card-text class="text-xs-center">
+                                        <v-btn
+                                                large
+                                                color="accent"
+                                                round
+                                        >Professor</v-btn>
+                                    </v-card-text>
+                                </material-card>
+                            </v-flex>
+                            <v-flex sm12
+                                    md4
+                            >
+                                <material-card class="v-card-profile ma-5">
+                                    <v-avatar
+                                            slot="offset"
+                                            class="mx-auto d-block"
+                                            size="130"
+                                    >
+                                        <img
+                                                src="/img/userTypes/staff.jpg"
+                                        >
+                                    </v-avatar>
+                                    <v-card-text class="text-xs-center">
+                                        <v-btn  large
+                                                color="secondary"
+                                                round
+                                        >Personal</v-btn>
+                                    </v-card-text>
+                                </material-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
             </v-flex>
-            <v-flex xs12>
-                <audit-log :entries="{{ $auditLogItems }}"></audit-log>
-            </v-flex>
+            {{--TEACHERS--}}
+            @if (($user->user_type_id === 1))
+                <v-flex md12
+                        sm12
+                        lg4>
+                    <dashboard-positions :user="{{ $user }}"></dashboard-positions>
+                </v-flex>
+            @endif
+            {{--<v-flex xs12>--}}
+            {{--User Type: {{ boolval($user->user_type_id) ? 'true' : 'false' }}--}}
+            {{--########--}}
+            {{--{{ $user }}--}}
+            {{--</v-flex>--}}
         </v-layout>
     </v-container>
-
-    <v-flex xs12>
-        <v-card>
-            <v-card-text class="px-0 mb-2 hidden-sm-and-down">
-                <h4>Removes</h4>
-                <ul>
-                    <li>S'ha de vigilar amb l'opció esborrar! Per exemple les places es poden esborrar si són errors clars i no etnen dades relacionades.
-                        Per exemple una plaça amb un horari assignat no té sentit que es pugui esborrar (si no s'esborra també l'horari)
-                    </li>
-                    <li></li>
-                </ul>
-                <h4>TODO</h4>
-                <ul>
-                    <li>Fitxers adjunts: fotocopies del DNI</li>
-                    <li>Foto proposada pel professorat: Alta professor -> Convertir a mides nostres? Requerir-les?</li>
-                    <li>Taula leaves per baixes i permisos. Al afegir un substitut es crea baixa automàticament</li>
-                    <li>Opció de marcar professor de baixa -> La plaça es posa com a pendent d'ocupar</li>
-                    <li>Fer aplicació que no sigui obligatori gestionar baixes?</li>
-                    <li>Botó settings: qui rep alertes/notificacions (emails) sobre canvir en professorat o places</li>
-                    <li>Datatables de dades secundaries en 4 blocs: dades personals (emails, tlefons,adreces) | HABILITACIONS (especialitats professorat) | Dades formació | Altres dades</li>
-                </ul>
-                <h4>Places</h4>
-                <ul>
-                    <li>Número total de places (de tipus professorat - no incloure conserges ni altres)</li>
-                    <li>Places per tipologia (Jornada sencera, mitja jornada, 1/3, etc). TODO: afegir camp ocupació (numero fins al 100)</li>
-                    <li>Número total de places per ocupació (Per exemple 110 places pero 10 són mitja jornada-> 105)</li>
-                    <li>Places no cobertes (places pendents assignar titular). Link cap a estes places per poder assignar. Alta nou professor es podria utilitzar esta info per facilitar l'alta nou profe?</li>
-                    <li>Places per tipologies: especialitat, familia (gràfiques tipus formatgets)</li>
-                    <li>Número total conserges, administratives</li>
-                    <li>Links a la gestió de places</li>
-                    <li>Link al llençol de professors</li>
-
-                </ul>
-                <h4>Profes</h4>
-                <ul>
-                    <li>TOP/MÉS IMPORTANT: Número profes pendents (podria ser fins i tot notificació/alerta per a cap estudis)</li>
-                    <li>Número total profes: inclou tot tipus de profes han passat pel centre durant l'any</li>
-                    <li>Número total profes actius: Hauria de coincidir amb nombre de places</li>
-                    <li>Número total profes baixa o permis: TODO crear taula leaves (per guardar baixes i permisos)</li>
-                    <li>Total profes per departaments/families/especialitats: gràfica tipus formatget???</li>
-                    <li>Número total profes per tipus (Funcionaris (definitius, provisionals, en pràctiques), Interins, Substituts). Formatget</li>
-                    <li>Profes sense plaça assignada (hauria de ser 0)</li>
-                </ul>
-                <h4>Profes. Estadístiques secundaries</h4>
-                <ul>
-                    <li>Alertes falta de dades: email personal, telèfons, adreça</li>
-                    <li>ES podria crear datatable dades professors es pugues veure quines dades falte</li>
-                    <li>Especialitat professors: habilitacions que tenen, qui no les té posades</li>
-                    <li>DADES FORMACIó: Titulació accés, Altres titulacions, Idiomes, Perfils?</li>
-                    <li>Altres dades: Data superació opos, anys inici serveis ensenyament (calcul antiguitat), any inici centre? destinació?</li>
-                </ul>
-            </v-card-text>
-        </v-card>
-    </v-flex>
-
 @endsection
-
-
