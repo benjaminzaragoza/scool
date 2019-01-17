82fac99a3e0ebafbfa4e10d70e6ef7755902d496
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-17 13:05:51 +0100

    Sync

--- resources/tenant_js/components/users/roles/RolesComponent.vue
+++ resources/tenant_js/components/users/roles/RolesComponent.vue
@@ -1,7 +1,13 @@
 <template>
     <span>
         <!--<role-add :roles="roles"></role-add>-->
-        <roles-list :roles="roles"></roles-list>
+        <v-container fluid grid-list-md text-xs-center>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <roles-list :roles="roles"></roles-list>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </span>
 </template>
 
