52c4a8f7ab1719b4a90ba3f073366d3824cd08c2
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-26 18:00:15 +0100

    Sync

--- resources/tenant_js/components/users/UsersListComponent.vue
+++ resources/tenant_js/components/users/UsersListComponent.vue
@@ -74,6 +74,17 @@
                             <div id="massive_actions" v-if="selected.length > 0" style="text-align: left;">
                                 <users-delete-multiple :users="selected" @deleted="selected=[];refresh(false)"></users-delete-multiple>
                             </div>
+
+                            <v-dialog
+                                    v-if="showUser"
+                                    v-model="showUser"
+                                    fullscreen
+                                    hide-overlay
+                                    transition="dialog-bottom-transition"
+                                    @keydown.esc.stop.prevent="showUser=false">
+                                <show-user :user="user" :users="users" @close="showUser=false"></show-user>
+                            </v-dialog>
+
                             <v-data-table
                                 v-model="selected"
                                 select-all
@@ -88,101 +99,101 @@
                                 rows-per-page-text="Usuaris per pàgina"
                                 :rows-per-page-items="[5,10,25,50,100,200,500,1000,{'text':'Tots','value':-1}]"
                             >
-                            <template slot="items" slot-scope="props">
-                                <tr>
-                                    <td>
-                                        <v-checkbox
-                                                v-model="props.selected"
-                                                primary
-                                                hide-details
-                                        ></v-checkbox>
-                                    </td>
-                                    <td class="text-xs-left cell">
-                                        {{ props.item.id }}
-                                    </td>
-                                    <td class="text-xs-left cell">
-                                        <user-avatar :hash-id="props.item.hashid"
-                                                     :alt="props.item.name"
-                                                     :user="props.item"
-                                                     :editable="true"
-                                                     :removable="true"
-                                        ></user-avatar>
-                                    </td>
-                                    <td class="text-xs-left cell" style="max-width: 150px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                        <user-edit-name :user="props.item" @saved="refresh(false)"></user-edit-name>
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ props.item.name }}</span>
-                                            <span>{{ props.item.name }}</span>
-                                        </v-tooltip>
-                                    </td>
-                                    <td class="text-xs-left cell" style="max-width: 175px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                        <user-edit-email :user="props.item" @saved="refresh(false)"></user-edit-email>
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ props.item.email }}</span>
-                                            <span>{{ props.item.email }}</span>
-                                        </v-tooltip>
-                                    </td>
-                                    <td class="text-xs-center cell">
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ formatBoolean(props.item.email_verified_at) }}</span>
-                                            <span v-if="props.item.email_verified_at">{{ props.item.email_verified_at }}</span>
-                                            <span v-else>{{ formatBoolean(props.item.email_verified_at) }}</span>
-                                        </v-tooltip>
-                                    <td class="text-xs-left cell" style="max-width: 125px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                        <span v-if="props.item.corporativeEmail">
+                                <template slot="items" slot-scope="props">
+                                    <tr>
+                                        <td>
+                                            <v-checkbox
+                                                    v-model="props.selected"
+                                                    primary
+                                                    hide-details
+                                            ></v-checkbox>
+                                        </td>
+                                        <td class="text-xs-left cell">
+                                            {{ props.item.id }}
+                                        </td>
+                                        <td class="text-xs-left cell">
+                                            <user-avatar :hash-id="props.item.hashid"
+                                                         :alt="props.item.name"
+                                                         :user="props.item"
+                                                         :editable="true"
+                                                         :removable="true"
+                                            ></user-avatar>
+                                        </td>
+                                        <td class="text-xs-left cell" style="max-width: 150px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                            <user-edit-name :user="props.item" @saved="refresh(false)"></user-edit-name>
                                             <v-tooltip bottom>
-                                                <span slot="activator"><a target="_blank" :href="'https://admin.google.com/u/3/ac/users/' + props.item.googleId">{{ props.item.corporativeEmail }}</a></span>
-                                                <span>{{ props.item.corporativeEmail }}</span>
+                                                <span slot="activator">{{ props.item.name }}</span>
+                                                <span>{{ props.item.name }}</span>
                                             </v-tooltip>
-                                        </span>
-                                        <manage-corporative-email-icon :user="props.item" @unassociated="refresh" @associated="refresh" @added="refresh"></manage-corporative-email-icon>
-                                    </td>
-                                    <td class="text-xs-left cell">
-                                        <inline-text-field-edit-dialog mask="###-###-###" placeholder="666777895" hint="9 números seguits sense codi de país" v-model="props.item" field="mobile" label="Mòbil" @save="refresh"></inline-text-field-edit-dialog>
-                                    </td>
-                                    <td class="text-xs-center cell">
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ formatBoolean(props.item.mobile_verified_at) }}</span>
-                                            <span v-if="props.item.mobile_verified_at">{{ props.item.mobile_verified_at }}</span>
-                                            <span v-else>{{ formatBoolean(props.item.mobile_verified_at) }}</span>
-                                        </v-tooltip>
-                                    </td>
-                                    <td class="text-xs-left cell">
-                                        <span v-if="props.item.moodleId">
+                                        </td>
+                                        <td class="text-xs-left cell" style="max-width: 175px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                            <user-edit-email :user="props.item" @saved="refresh(false)"></user-edit-email>
                                             <v-tooltip bottom>
-                                                <span slot="activator"><a target="_blank" :href="'https://www.iesebre.com/moodle/user/profile.php?id=' + props.item.moodleId">{{ props.item.moodleId }}</a></span>
-                                                <span>{{ props.item.moodleId }} | {{ props.item.moodleUsername }}</span>
+                                                <span slot="activator">{{ props.item.email }}</span>
+                                                <span>{{ props.item.email }}</span>
                                             </v-tooltip>
-                                        </span>
-                                        <manage-moodle-user-icon :user="props.item" @unassociated="refresh(false)" @associated="refresh(false)"></manage-moodle-user-icon>
-                                    </td>
-                                    <td class="text-xs-left cell">{{ formatUserType(props.item.user_type_id) }}</td>
-                                    <td class="text-xs-left cell">{{ formatBoolean(props.item.admin) }}</td>
-                                    <td class="text-xs-left cell" style="max-width: 150px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                        <user-roles-manage-button :user="props.item" @added="refresh(false)" @removed="refresh(false)"></user-roles-manage-button>
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ formatRoles(props.item) }}</span>
-                                            <span>{{ formatRoles(props.item) }}</span>
-                                        </v-tooltip>
-                                    </td>
-                                    <td class="text-xs-left cell">
-                                        <v-tooltip bottom>
-                                            <span slot="activator">{{ props.item.last_login_diff }}</span>
-                                            <span>{{ props.item.last_login_ip }} | {{ props.item.last_login_formatted }}</span>
-                                        </v-tooltip>
-                                    </td>
-                                    <td class="text-xs-left cell" v-html="props.item.formatted_created_at_diff" :title="props.item.formatted_created_at"></td>
-                                    <td class="text-xs-left cell" :title="props.item.formatted_updated_at">{{props.item.formatted_updated_at_diff}}</td>
-                                    <td class="text-xs-left cell">
-                                        <show-user-icon :user="props.item" :users="users" :current-user="user"></show-user-icon>
-                                        <user-emails :user="props.item"></user-emails>
-                                        <!--<user-send-welcome-email :user="props.item"></user-send-welcome-email>-->
-                                        <!--<user-send-reset-password-email :user="props.item"></user-send-reset-password-email>-->
-                                        <!--<user-send-confirmation-email :user="props.item"></user-send-confirmation-email>-->
-                                        <user-delete :user="props.item"></user-delete>
-                                    </td>
-                                </tr>
-                            </template>
+                                        </td>
+                                        <td class="text-xs-center cell">
+                                            <v-tooltip bottom>
+                                                <span slot="activator">{{ formatBoolean(props.item.email_verified_at) }}</span>
+                                                <span v-if="props.item.email_verified_at">{{ props.item.email_verified_at }}</span>
+                                                <span v-else>{{ formatBoolean(props.item.email_verified_at) }}</span>
+                                            </v-tooltip>
+                                        <td class="text-xs-left cell" style="max-width: 125px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                            <span v-if="props.item.corporativeEmail">
+                                                <v-tooltip bottom>
+                                                    <span slot="activator"><a target="_blank" :href="'https://admin.google.com/u/3/ac/users/' + props.item.googleId">{{ props.item.corporativeEmail }}</a></span>
+                                                    <span>{{ props.item.corporativeEmail }}</span>
+                                                </v-tooltip>
+                                            </span>
+                                            <manage-corporative-email-icon :user="props.item" @unassociated="refresh" @associated="refresh" @added="refresh"></manage-corporative-email-icon>
+                                        </td>
+                                        <td class="text-xs-left cell">
+                                            <inline-text-field-edit-dialog mask="###-###-###" placeholder="666777895" hint="9 números seguits sense codi de país" v-model="props.item" field="mobile" label="Mòbil" @save="refresh"></inline-text-field-edit-dialog>
+                                        </td>
+                                        <td class="text-xs-center cell">
+                                            <v-tooltip bottom>
+                                                <span slot="activator">{{ formatBoolean(props.item.mobile_verified_at) }}</span>
+                                                <span v-if="props.item.mobile_verified_at">{{ props.item.mobile_verified_at }}</span>
+                                                <span v-else>{{ formatBoolean(props.item.mobile_verified_at) }}</span>
+                                            </v-tooltip>
+                                        </td>
+                                        <td class="text-xs-left cell">
+                                            <span v-if="props.item.moodleId">
+                                                <v-tooltip bottom>
+                                                    <span slot="activator"><a target="_blank" :href="'https://www.iesebre.com/moodle/user/profile.php?id=' + props.item.moodleId">{{ props.item.moodleId }}</a></span>
+                                                    <span>{{ props.item.moodleId }} | {{ props.item.moodleUsername }}</span>
+                                                </v-tooltip>
+                                            </span>
+                                            <manage-moodle-user-icon :user="props.item" @unassociated="refresh(false)" @associated="refresh(false)"></manage-moodle-user-icon>
+                                        </td>
+                                        <td class="text-xs-left cell">{{ formatUserType(props.item.user_type_id) }}</td>
+                                        <td class="text-xs-left cell">{{ formatBoolean(props.item.admin) }}</td>
+                                        <td class="text-xs-left cell" style="max-width: 150px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                            <user-roles-manage-button :user="props.item" @added="refresh(false)" @removed="refresh(false)"></user-roles-manage-button>
+                                            <v-tooltip bottom>
+                                                <span slot="activator">{{ formatRoles(props.item) }}</span>
+                                                <span>{{ formatRoles(props.item) }}</span>
+                                            </v-tooltip>
+                                        </td>
+                                        <td class="text-xs-left cell">
+                                            <v-tooltip bottom>
+                                                <span slot="activator">{{ props.item.last_login_diff }}</span>
+                                                <span>{{ props.item.last_login_ip }} | {{ props.item.last_login_formatted }}</span>
+                                            </v-tooltip>
+                                        </td>
+                                        <td class="text-xs-left cell" v-html="props.item.formatted_created_at_diff" :title="props.item.formatted_created_at"></td>
+                                        <td class="text-xs-left cell" :title="props.item.formatted_updated_at">{{props.item.formatted_updated_at_diff}}</td>
+                                        <td class="text-xs-left cell">
+                                            <show-user-icon :user="props.item" :users="users"></show-user-icon>
+                                            <user-emails :user="props.item"></user-emails>
+                                            <!--<user-send-welcome-email :user="props.item"></user-send-welcome-email>-->
+                                            <!--<user-send-reset-password-email :user="props.item"></user-send-reset-password-email>-->
+                                            <!--<user-send-confirmation-email :user="props.item"></user-send-confirmation-email>-->
+                                            <user-delete :user="props.item"></user-delete>
+                                        </td>
+                                    </tr>
+                                </template>
                             </v-data-table>
                         </v-card>
 
@@ -233,6 +244,7 @@ import { mapGetters } from 'vuex'
 import * as mutations from '../../store/mutation-types'
 import * as actions from '../../store/action-types'
 import ShowUserIcon from './ShowUserIconComponent.vue'
+import ShowUser from './ShowUserComponent.vue'
 import UserAvatar from '../ui/UserAvatarComponent'
 import UserSendWelcomeEmail from './UserSendWelcomeEmail'
 import UserSendResetPasswordEmail from './UserSendResetPasswordEmail'
@@ -378,6 +390,7 @@ export default {
     'user-send-reset-password-email': UserSendResetPasswordEmail,
     'user-send-confirmation-email': UserSendConfirmationEmail,
     'show-user-icon': ShowUserIcon,
+    'show-user': ShowUser,
     'manage-corporative-email-icon': ManageCorporativeEmailIcon,
     'manage-moodle-user-icon': ManageMoodleUserIcon,
     'user-avatar': UserAvatar,
@@ -392,6 +405,7 @@ export default {
   },
   data () {
     return {
+      showUser: false,
       selected: [],
       search: '',
       refreshing: false,
@@ -486,6 +500,9 @@ export default {
       3: 'Conserge',
       4: 'Administratiu',
       5: 'Familiar'
+    }
+    if (this.user) {
+      this.showUser = true
     }
   }
 }
