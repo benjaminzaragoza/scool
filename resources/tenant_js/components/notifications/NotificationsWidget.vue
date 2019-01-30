c20c6f4264fca79d1bdbe4d93b7fef9c07cae6bc
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-01-30 13:04:21 +0100

    Sync

--- resources/tenant_js/components/notifications/NotificationsWidget.vue
+++ resources/tenant_js/components/notifications/NotificationsWidget.vue
@@ -2,17 +2,19 @@
     <v-menu offset-y>
         <v-badge slot="activator" left overlap color="error" class="ml-3 mr-2">
             <span slot="badge" v-text="amount"></span>
-            <v-btn icon color="white">
+            <v-btn icon color="white" :loading="loading" :disabled="loading">
                 <v-icon :large="large" color="primary">notifications</v-icon>
             </v-btn>
         </v-badge>
         <v-list>
-            <v-list-tile
+            <v-list-tile v-if="dataNotifications.length > 0"
                     v-for="(dataNotification, index) in dataNotifications"
                     :key="index"
-                    @click=""
             >
                 <v-list-tile-title style="max-width: 450px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ dataNotification.title }}</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+                <v-list-tile-title>No hi ha cap notificació pendent</v-list-tile-title>
             </v-list-tile>
         </v-list>
     </v-menu>
@@ -24,7 +26,8 @@ export default {
   name: 'NotificationsWidget',
   data () {
     return {
-      dataNotifications: this.notifications
+      dataNotifications: [],
+      loading: false
     }
   },
   props: {
@@ -44,23 +47,18 @@ export default {
     }
   },
   created () {
-    this.dataNotifications = [
-      {
-        title: 'Notification 1'
-      },
-      {
-        title: 'Notification 2'
-      },
-      {
-        title: 'Notification 3'
-      },
-      {
-        title: 'Notification 4'
-      },
-      {
-        title: 'Notification 4 amb un text més llarg que un dia sense pa i bla bla bla bla bla bla bla bla bla bla bla bla Notification 4 amb un text més llarg que un dia sense pa i bla l bla bla bla bla bla bla bla bla bla bla bla'
-      }
-    ]
+    if (this.notifications) {
+      this.dataNotifications = this.notifications
+    } else {
+      this.loading = true
+      window.axios.get('/api/v1/user/notifications').then((response) => {
+        this.dataNotifications = response.data
+        this.loading = false
+      }).catch(error => {
+        this.$snackbar.showError(error)
+        this.loading = false
+      })
+    }
   }
 }
 </script>
