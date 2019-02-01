00e4c98d86b83f9edd6c8bd4bd679d0a99299dda
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-02-01 11:28:29 +0100

    Sync

--- resources/tenant_js/components/users/UsersUserTypeManagement.vue
+++ resources/tenant_js/components/users/UsersUserTypeManagement.vue
@@ -1,10 +1,66 @@
 <template>
-    <a :href="userTypeLink()" target="_blank">{{ formatUserType() }}</a>
+    <span>
+        <span v-if="user.user_type_id">
+            <a :href="userTypeLink()" target="_blank" class="mr-0">{{ formatUserType() }}</a>
+            <v-tooltip bottom class="ml-0">
+                <v-btn flat slot="activator" icon small color="success" @click="dialog=true">
+                    <v-icon small>edit</v-icon>
+                </v-btn>
+                <span>Modificar el tipus d'usuari</span>
+            </v-tooltip>
+        </span>
+        <span v-else>
+            <v-tooltip bottom>
+                <v-btn flat slot="activator" icon small color="primary" @click="dialog=true">
+                    <v-icon small>add</v-icon>
+                </v-btn>
+                <span>Establir el tipus d'usuari</span>
+            </v-tooltip>
+        </span>
+        <v-dialog
+                v-if="dialog"
+                v-model="dialog"
+                width="500"
+        >
+            <v-toolbar color="primary" dense>
+                <v-toolbar-title class="white--text title">Tipus d'usuari</v-toolbar-title>
+            </v-toolbar>
+            <v-card>
+              <v-card-text>
+                TODO SELECT TIPUS USUARI
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                          class="grey--text"
+                          flat
+                          @click="dialog = false"
+                  >
+                  Cancel·lar
+                </v-btn>
+                <v-btn
+                        color="primary"
+                        @click="dialog = false"
+                >
+                  Canviar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+    </span>
 </template>
 
 <script>
 export default {
   name: 'UsersUserTypeManagement',
+  data () {
+    return {
+      dialog: false
+    }
+  },
   props: {
     user: {
       type: Object,
