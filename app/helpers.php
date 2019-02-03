ade622e1d184153adda60d7ddb8a74c7e5b6456e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-02-03 13:42:18 +0100

    Sync

--- app/helpers.php
+++ app/helpers.php
@@ -9254,7 +9254,7 @@ if (! function_exists('create_sample_people')) {
             'givenName' => 'Pepe',
             'sn1' => 'Pardo',
             'sn2' => 'Jeans',
-            'birthdate' => Carbon::createFromFormat('d-m-Y','02-03-1978')->format('d-m-Y'),
+            'birthdate' => Carbon::createFromFormat('d-m-Y','02-03-1978'),
 //            'birthplace_id' => 'Jeans',
             'gender' => 'Home',
             'civil_status' => 'Solter/a',
