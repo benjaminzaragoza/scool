5c30a6282872e582ce7a964c7e65db67334f3a4e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-02-24 16:33:56 +0100

    Sync

---
+++ app/Listeners/Users/Password/ChangeLdapPassword.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners\Users\Password;
+
+use App\Models\Incident;
+use Cache;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+/**
+ * Class ChangeLdapPassword
+ * @package App\Listeners
+ */
+class ChangeLdapPassword
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+
+    }
+}
---
+++ app/Listeners/Users/Password/SendPasswordChangedEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners\Users\Password;
+
+use App\Models\Incident;
+use Cache;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+/**
+ * Class SendPasswordChangedEmail
+ * @package App\Listeners
+ */
+class SendPasswordChangedEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+
+    }
+}
